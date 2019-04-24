940bf7afe1e49b54ae0cae567f86c6165d259ccc
Jay <hotwingsfromtroy@outlook.com>
2019-04-24 18:58:22 +0530

    Web Frameworks Mailing App, second commit

--- aether/settings.py
+++ aether/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'aether.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'Templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
